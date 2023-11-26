d210b579029e02848436007a342be651720e46d0
Shreyesh Boppana <shreyeshboppana@Shreyeshs-MacBook-Pro.local>
2023-11-26 23:37:48 +0530

    All changed

--- server.py
+++ server.py
@@ -6,10 +6,10 @@ app = Flask("Sentiment Analyzer")
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
     text_to_analyze = request.args.get('textToAnalyze')
-    '''response = sentiment_analyzer(text_to_analyze)
+    response = sentiment_analyzer(text_to_analyze)
     label = response['label']
-    score = response['score']'''
-    return text_to_analyze
+    score = response['score']
+    return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
 
 @app.route("/")
 def render_index_page():
